83e67b69b205da7b69418162ff91e4652c69bf8f
Morevq <72866082+Morevq@users.noreply.github.com>
2022-05-28 10:49:36 +0300

    ?

--- SimpleGame/MainWindow.xaml.cs
+++ SimpleGame/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace SimpleGame
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            WindowOfGame window = new WindowOfGame();
+            Level3 window = new Level3();
             window.Show();
             this.Close();
         }
